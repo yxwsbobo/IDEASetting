ce39f9d2061243985fed8ce750861e80ca7a41eb
yxwsbobo <yxwsbobo@gmail.com>
2019-06-11 13:51:40 +0800

    CL-2019.1.4 <kin@kin-pc Create baseRefactoring.xml, ide.general.xml, project.default.xml, laf.xml, embedded-support.xml, Default.xml, find.xml, filetypes.xml, proxy.settings.xml, cpp.toolchains.xml, colors.scheme.xml, C++ Class Header.h, _@user_Darcula.icls, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,15 @@
+#parse("C File Header.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+${NAMESPACES_OPEN}
+namespace ${PROJECT_NAME} {
+
+class ${NAME} {
+
+};
+
+}
+${NAMESPACES_CLOSE}
+
+#[[#endif]]# //${INCLUDE_GUARD}
